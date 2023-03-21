f6254ed28d0cb0a5316800868c8067c57111d782
Robson Lourenço <robinhosl2008@gmail.com>
2023-03-21 07:30:42 -0300

    Primeiro commit.

---
+++ src/Model/Leilao.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BoasPraticas\Leilao\Model;
+
+class Leilao
+{
+    /** @var Lance[] */
+    private $lances;
+    /** @var string */
+    private $descricao;
+
+    public function __construct(string $descricao)
+    {
+        $this->descricao = $descricao;
+        $this->lances = [];
+    }
+
+    public function recebeLance(Lance $lance)
+    {
+        $this->lances[] = $lance;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function recuperaLances(): array
+    {
+        return $this->lances;
+    }
+}
