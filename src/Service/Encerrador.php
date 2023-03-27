35060d71f63c6e3071e2f5a0fa3f09e9ed73ac7e
Robson Lourenço <robinhosl2008@gmail.com>
2023-03-27 18:41:08 -0300

    Finalizando teste com mocks

--- src/Service/Encerrador.php
+++ src/Service/Encerrador.php
@@ -8,13 +8,16 @@ class Encerrador
 {
     /** @var LeilaoDao */
     protected $dao;
+    /** @var EnviadorEmail */
+    protected $enviadorEmail;
 
     /**
      * Método construtor.
      **/
-    public function __construct(LeilaoDao $dao)
+    public function __construct(LeilaoDao $dao, EnviadorEmail $enviadorEmail)
     {
         $this->dao = $dao;
+        $this->enviadorEmail = $enviadorEmail;
     }
 
     public function encerra()
@@ -23,8 +26,13 @@ class Encerrador
 
         foreach ($leiloes as $leilao) {
             if ($leilao->temMaisDeUmaSemana()) {
-                $leilao->finalizaLeilao();
-                $this->dao->atualiza($leilao);
+                try {
+                    $leilao->finalizaLeilao();
+                    $this->dao->atualiza($leilao);
+                    $this->enviadorEmail->notivicarPorEmail($leilao);
+                } catch (\DomainException $e) {
+                    error_log($e->getMessage());
+                }
             }
         }
     }
--- tests/EncerradorTest.php
+++ tests/EncerradorTest.php
@@ -5,6 +5,7 @@ namespace BoasPraticas\Leilao\Tests;
 use BoasPraticas\Leilao\Dao\LeilaoDao;
 use BoasPraticas\Leilao\Model\Leilao;
 use BoasPraticas\Leilao\Service\Encerrador;
+use BoasPraticas\Leilao\Service\EnviadorEmail;
 use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
@@ -37,34 +38,72 @@ use PHPUnit\Framework\TestCase;
 
 class EncerradorTest extends TestCase
 {
-    public function testLeilaoComMaisDeUmaSemanaDevemSerEncerrados()
+        private $encerrador;
+        private $leilaoFiat147;
+        private $leilaoVariant;
+        /** @var MockObject */
+        private $enviadorEmail;
+
+    public function setUp(): void
     {
         // arrange
-        $leilao2 = new Leilao(
+        $this->leilaoVariant = new Leilao(
             "Variant 1972 teste-dev",
             new DateTimeImmutable('10 days ago')
         );
 
-        $leilao1 = new Leilao(
+        $this->leilaoFiat147 = new Leilao(
             "Fiat 147 teste-dev",
             new DateTimeImmutable("8 days ago")
         );
 
         $leilaoDao = $this->createMock(LeilaoDao::class);
-        $leilaoDao->method('recuperarNaoFinalizados')->willReturn([$leilao1, $leilao2]);
+        $leilaoDao->method('recuperarNaoFinalizados')->willReturn([
+            $this->leilaoVariant, 
+            $this->leilaoFiat147
+        ]);
         // $leilaoDao->expects($this->exactly(2))->method('atualiza')->withConsecutive(
         //     [$leilao1], 
         //     [$leilao2]
         // );
 
         // act
-        $encerrador = new Encerrador($leilaoDao);
-        $encerrador->encerra();
-        $leiloes = [$leilao1, $leilao2];
+        $this->enviadorEmail = $this->createMock(EnviadorEmail::class);
+        $this->encerrador = new Encerrador($leilaoDao, $this->enviadorEmail);
+    }
+
+    public function testLeilaoComMaisDeUmaSemanaDevemSerEncerrados()
+    {
+        $this->encerrador->encerra();
+        $leiloes = [$this->leilaoVariant, $this->leilaoVariant];
 
         // assert
         self::assertCount(2, $leiloes);
         self::assertTrue($leiloes[0]->recuperaStatusLeilao());
         self::assertTrue($leiloes[1]->recuperaStatusLeilao());
+    }
+
+    public function testProcessoDeEncerramentoDeveContinuarOcorrendoMesmoComOErroDeEnvioDeEmail()
+    {
+        $e = new \DomainException("Erro ao enviar e-mail");
+        $this->enviadorEmail
+            ->expects($this->exactly(2))
+            ->method('notivicarPorEmail')
+            ->willThrowException($e);
+
+        $this->encerrador->encerra();
+
+    }
+
+    public function testDeveEnviarEmailSomenteAposFinalizadoOLeilao()
+    {
+        $this->enviadorEmail
+            ->expects($this->exactly(2))
+            ->method('notivicarPorEmail')
+            ->willReturnCallback(function (Leilao $leilao) {
+                self::assertTrue($leilao->recuperaStatusLeilao());
+            });
+
+        $this->encerrador->encerra();
     }
 }
