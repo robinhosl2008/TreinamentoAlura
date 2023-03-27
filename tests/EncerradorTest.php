5bcb08dd53b2732d8280f339487692c7490c4d24
Robson Lourenço <robinhosl2008@gmail.com>
2023-03-27 17:19:15 -0300

    mock

--- tests/EncerradorTest.php
+++ tests/EncerradorTest.php
@@ -8,41 +8,35 @@ use BoasPraticas\Leilao\Service\Encerrador;
 use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 
-class LeilaoDaoMock extends LeilaoDao
-{
-    private $leiloes = [];
+// class LeilaoDaoMock extends LeilaoDao
+// {
+//     private $leiloes = [];
 
-    public function salva(Leilao $leilao): void
-    {
-        $this->leiloes[] = $leilao;
-    }
+//     public function salva(Leilao $leilao): void
+//     {
+//         $this->leiloes[] = $leilao;
+//     }
 
-    public function recuperarNaoFinalizados(): array
-    {
-        return array_filter($this->leiloes, function(Leilao $leilao) {
-            return !$leilao->recuperaStatusLeilao();
-        });
-    }
+//     public function recuperarNaoFinalizados(): array
+//     {
+//         return array_filter($this->leiloes, function(Leilao $leilao) {
+//             return !$leilao->recuperaStatusLeilao();
+//         });
+//     }
 
-    public function recuperarFinalizados(string $str): array
-    {
-        return array_filter($this->leiloes, function(Leilao $leilao) {
-            return $leilao->recuperaStatusLeilao();
-        });
-    }
+//     public function recuperarFinalizados(string $str): array
+//     {
+//         return array_filter($this->leiloes, function(Leilao $leilao) {
+//             return $leilao->recuperaStatusLeilao();
+//         });
+//     }
 
-    public function atualiza(Leilao $leilao)
-    {}
-}
+//     public function atualiza(Leilao $leilao)
+//     {}
+// }
 
 class EncerradorTest extends TestCase
 {
-    public function tearDown(): void
-    {
-        // $leilaoDao = new LeilaoDao();
-        // $leilaoDao->removeLeiloes("teste-dev", null);
-    }
-
     public function testLeilaoComMaisDeUmaSemanaDevemSerEncerrados()
     {
         // arrange
@@ -56,18 +50,21 @@ class EncerradorTest extends TestCase
             new DateTimeImmutable("8 days ago")
         );
 
-        $leilaoDao = new LeilaoDaoMock();
-        $leilaoDao->salva($leilao1);
-        $leilaoDao->salva($leilao2);
+        $leilaoDao = $this->createMock(LeilaoDao::class);
+        $leilaoDao->method('recuperarNaoFinalizados')->willReturn([$leilao1, $leilao2]);
+        // $leilaoDao->expects($this->exactly(2))->method('atualiza')->withConsecutive(
+        //     [$leilao1], 
+        //     [$leilao2]
+        // );
 
         // act
         $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
-        $leiloes = $leilaoDao->recuperarFinalizados("teste-dev");
+        $leiloes = [$leilao1, $leilao2];
 
         // assert
         self::assertCount(2, $leiloes);
-        self::assertEquals("Fiat 147 teste-dev", $leiloes[0]->recuperaDescricao());
-        self::assertEquals("Variant 1972 teste-dev", $leiloes[1]->recuperaDescricao());
+        self::assertTrue($leiloes[0]->recuperaStatusLeilao());
+        self::assertTrue($leiloes[1]->recuperaStatusLeilao());
     }
 }
